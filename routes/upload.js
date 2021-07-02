688b883fc5a62c99017acf15e390a8eb9c756185
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-07-02 19:18:55 +1000

    Added basic file upload

--- routes/upload.js
+++ routes/upload.js
@@ -4,8 +4,23 @@ const router = express.Router();
 // Controller modules
 const uploads_controller = require('../controllers/uploads_controller')
 
+// Multer
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+  }
+})
+ 
+const upload = multer({ storage: storage })
+
 // Routes
 router.get('/', uploads_controller.upload_get)
-router.post('/', uploads_controller.upload_post)
+router.post('/', upload.single('upload'), function(req, res) {
+  res.send("File uploaded.");
+});
 
 module.exports = router;
