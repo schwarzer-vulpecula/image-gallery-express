38969dd27ae2ce982a7029f3e1d298374a92662f
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-07-02 17:36:57 +1000

    Added multer upload to controller

--- controllers/uploads_controller.js
+++ controllers/uploads_controller.js
@@ -1,5 +1,17 @@
 const Entry = require('../models/entry');
 
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
 exports.upload_get = function (req, res){
     res.send('NOT IMPLEMENTED: Get Upload');
 }
