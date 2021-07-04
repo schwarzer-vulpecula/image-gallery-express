ccb3236145dd0d1ee5b67d19298bfb0f5d82aab6
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-07-04 17:57:06 +1000

    Updated gallery controller

--- controllers/gallery_controller.js
+++ controllers/gallery_controller.js
@@ -9,18 +9,17 @@ exports.gallery_show = function (req, res){
     .exec(function (err, entry) {
       if (entry==null) { // No results.
         res.render('gallery_error');
-        return;
       }
-      // Successful, so render.
-      console.log(entry.image_name);
-      // Change the date to a more readable format
-      const formattedDate = entry.createdAt.toLocaleString("en-US", {
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-        hour: "numeric",
-        minute: "2-digit"
-      });
-      res.render('gallery_show', { entry: entry, time: formattedDate });
+      else {
+        // Change the date to a more readable format
+        const formattedDate = entry.createdAt.toLocaleString("en-US", {
+          day: "numeric",
+          month: "short",
+          year: "numeric",
+          hour: "numeric",
+          minute: "2-digit"
+        });
+        res.render('gallery_show', { entry: entry, time: formattedDate });
+      }
     })
 }
