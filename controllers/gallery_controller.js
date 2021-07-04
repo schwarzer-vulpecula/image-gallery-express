033efb18456ea1d53977c69a5050d0ff5d211193
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-07-04 17:36:42 +1000

    View for displaying individual image added

--- controllers/gallery_controller.js
+++ controllers/gallery_controller.js
@@ -5,5 +5,22 @@ exports.gallery_index = function (req, res){
 }
 
 exports.gallery_show = function (req, res){
-  res.send('NOT IMPLEMENTED: Entry detail: ' + req.params.id);
+  Entry.findOne({ image_name: req.params.id })
+    .exec(function (err, entry) {
+      if (entry==null) { // No results.
+        res.render('gallery_error');
+        return;
+      }
+      // Successful, so render.
+      console.log(entry.image_name);
+      // Change the date to a more readable format
+      const formattedDate = entry.createdAt.toLocaleString("en-US", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+        hour: "numeric",
+        minute: "2-digit"
+      });
+      res.render('gallery_show', { entry: entry, time: formattedDate });
+    })
 }
