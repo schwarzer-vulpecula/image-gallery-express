bec1422e765b8985b49e083bf4e7c653fb0c8a99
Schwarzer Vulpecula <schwarzer.vulpecula@gmail.com>
2021-07-06 14:14:15 +1000

    Added user input sanitation

--- controllers/uploads_controller.js
+++ controllers/uploads_controller.js
@@ -1,11 +1,15 @@
 const Entry = require('../models/entry');
 
+const squish = function (string){
+  return string.replace(/\s+/g,' ').trim();
+}
+
 exports.upload_get = function (req, res){
   res.render('upload', { title: 'Upload an Image' } );
 }
 
 exports.upload_post = function (req, res){
-  const entry = new Entry({ image_name: req.file.filename, original_name: req.file.originalname, description: req.body.description, public_image: req.body.public_image });
+  const entry = new Entry({ image_name: req.file.filename, original_name: squish(req.file.originalname), description: squish(req.body.description), public_image: req.body.public_image });
   entry.save().then(() => console.log('Entry successfully created'));
   const url = '../gallery/' + req.file.filename;
   res.redirect(url)
